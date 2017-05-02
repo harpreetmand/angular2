c1c2d2fe413cdefb2138d260aabc3ab051f1ac80
Harpreet Mand <harpreet@limeadvertising.com>
2017-05-02 10:46:29 -0400

    completed simple routing example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { NavbarComponent }  from './components/navbar/navbar.component';
+import { JumbotronComponent }  from './components/jumbotron/jumbotron.component';
+import { AboutComponent }  from './components/pages/about.component';
+import { HomeComponent }  from './components/pages/home.component';
+import { routing }  from './app.routing';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
      ],
-  declarations: [ AppComponent, NavbarComponent ],
+  declarations: [ AppComponent, 
+                  NavbarComponent, 
+                  JumbotronComponent,
+                  AboutComponent,
+                  HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
